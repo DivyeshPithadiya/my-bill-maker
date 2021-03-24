dcc745b9a2bb9bcff8a166f6491cfcd3080741cb
DivyeshPithadiya <divyeshpithadiya3@gmail.com>
2021-03-25 01:42:50 +0530

    ReqMapAdded

--- src/main/java/com/billMaker/billSystemProject/Controller/Controller.java
+++ src/main/java/com/billMaker/billSystemProject/Controller/Controller.java
@@ -3,6 +3,7 @@ package com.billMaker.billSystemProject.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import com.billMaker.billSystemProject.TableClasses.Users;
 //class CrossOriginConfig {
 
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class Controller {
 	@Autowired
