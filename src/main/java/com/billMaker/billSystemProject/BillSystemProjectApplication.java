99fa4e046d0e3aadda1f4a1d7d3ad956c54777e2
DivyeshPithadiya <divyeshpithadiya3@gmail.com>
2021-03-25 13:00:56 +0530

    AutoConfigAdded

--- src/main/java/com/billMaker/billSystemProject/BillSystemProjectApplication.java
+++ src/main/java/com/billMaker/billSystemProject/BillSystemProjectApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BillSystemProjectApplication {
@@ -14,14 +17,20 @@ public class BillSystemProjectApplication {
 		SpringApplication.run(BillSystemProjectApplication.class, args);
 	}
 	
-	  @Bean
-	   public WebMvcConfigurer corsConfigurer() {
-	      return new WebMvcConfigurer() {
-	         @Override
-	         public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/**").allowedOrigins("https://my-bill-system.herokuapp.com");
-	         }
-	      };
-	   }
+	@SuppressWarnings("deprecation")
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+				.allowedOrigins("https://my-bill-system.herokuapp.com/")
+				.allowedMethods("PUT", "DELETE","POST")
+				.allowedHeaders("header1", "header2", "header3")
+				.exposedHeaders("header1", "header2")
+				.allowCredentials(false).maxAge(3600);
+		}
+	}
 
 }
--- src/main/java/com/billMaker/billSystemProject/Controller/Controller.java
+++ src/main/java/com/billMaker/billSystemProject/Controller/Controller.java
@@ -16,7 +16,7 @@ import com.billMaker.billSystemProject.TableClasses.Bill_details;
 import com.billMaker.billSystemProject.TableClasses.Users;
 
 
-
+@CrossOrigin("https://my-bill-system.herokuapp.com/")
 @RestController
 public class Controller {
 	@Autowired
