9a85dd7c21a9cae13051d8a598f0694b5df58bd3
DivyeshPithadiya <divyeshpithadiya3@gmail.com>
2021-03-25 17:15:37 +0530

    AutoConfigAdded

--- src/main/java/com/billMaker/billSystemProject/BillSystemProjectApplication.java
+++ src/main/java/com/billMaker/billSystemProject/BillSystemProjectApplication.java
@@ -3,10 +3,15 @@ package com.billMaker.billSystemProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = {"/billSystemProject/src/main/java/com/billMaker/billSystemProject/Controller/Controller.java"})
+
 public class BillSystemProjectApplication {
 
 	public static void main(String[] args) {
