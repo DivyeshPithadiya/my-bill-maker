4aaae1f2c19e08ea8f17c8df1097ba48ccbc7262
DivyeshPithadiya <divyeshpithadiya3@gmail.com>
2021-03-25 10:07:32 +0530

    CSRFDisabled

--- src/main/java/com/billMaker/billSystemProject/BillSystemProjectApplication.java
+++ src/main/java/com/billMaker/billSystemProject/BillSystemProjectApplication.java
@@ -2,12 +2,26 @@ package com.billMaker.billSystemProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class BillSystemProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillSystemProjectApplication.class, args);
+	}
+	
+	@EnableWebSecurity
+	@Configuration
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http.csrf().disable();
+	  }
 	}
 
 }
